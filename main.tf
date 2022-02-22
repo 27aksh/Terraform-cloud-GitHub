f08b94076b843f5ff0d160f6243d0bbad9a65b26
27aksh <97507713+27aksh@users.noreply.github.com>
2022-02-23 00:26:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 terraform {
-    required_version = "~> 1.0.3" // added latest version
+    required_version = "~> 1.1.4" // added latest version
     required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 4.1"
      }
     }
    }
 
 provider "aws" {
-       # region = "us-east-1" 
-        profile = "default"
+        region = "us-east-1" 
+       # profile = "default"
 	}
 
 resource "aws_instance" "Raminfra" {
