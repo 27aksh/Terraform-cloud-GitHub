059ca1c658ceca799287a1ceda13c9322bfae6cd
27aksh <97507713+27aksh@users.noreply.github.com>
2022-02-23 13:11:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 terraform {
-    required_version = "~> 1.1.4" // added latest version
+    required_version = "~> 1.1.5" // added latest version
     required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.1"
+      version = "~> 4.2"
      }
     }
    }
 provider "aws" {
-        region = "us-east-1" 
-        #profile = "default"
+        #region = "us-east-1" 
+        profile = "default"
 	}
 
 resource "aws_instance" "akshthainfra" {
